9fb79f39b3f7b8f308c671fd75a83deb09c1c99d
Michael Trindade <michael.trindade@hotmail.com>
2022-06-07 23:06:24 -0400

    Implementando jakarta persistence na classe pessoa

--- src/main/java/com/mts/crudprojetosweb/domain/model/Pessoa.java
+++ src/main/java/com/mts/crudprojetosweb/domain/model/Pessoa.java
@@ -1,5 +1,10 @@
 package com.mts.crudprojetosweb.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +12,14 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Pessoa {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String email;
 	private String telefone;
