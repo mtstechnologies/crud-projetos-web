0119536cc3c1af308338cd9ebe6e004730ff70a1
Michael Trindade <michael.trindade@hotmail.com>
2022-06-07 22:00:52 -0400

    Projeto criado

---
+++ src/main/java/com/mts/crudprojetosweb/CrudProjetosWebApplication.java
@@ -0,0 +1,13 @@
+package com.mts.crudprojetosweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProjetosWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProjetosWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mts/crudprojetosweb/CrudProjetosWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mts.crudprojetosweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProjetosWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
