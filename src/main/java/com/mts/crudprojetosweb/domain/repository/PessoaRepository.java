223e3cd9f6e60be5cd2259aff0722ffedf531223
Michael Trindade <michael.trindade@hotmail.com>
2022-06-10 23:24:45 -0400

    Serviço criado para não permitir cadastro com e-mail repetido

--- src/main/java/com/mts/crudprojetosweb/domain/repository/PessoaRepository.java
+++ src/main/java/com/mts/crudprojetosweb/domain/repository/PessoaRepository.java
@@ -1,5 +1,8 @@
 package com.mts.crudprojetosweb.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,7 @@ import com.mts.crudprojetosweb.domain.model.Pessoa;
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
 
+	List<Pessoa> findByNome(String nome);
+	List<Pessoa> findByNomeContaining(String nome);
+	Optional<Pessoa> findByEmail(String email);
 }
--- src/main/java/com/mts/crudprojetosweb/domain/service/PessoaService.java
+++ src/main/java/com/mts/crudprojetosweb/domain/service/PessoaService.java
@@ -3,6 +3,7 @@ package com.mts.crudprojetosweb.domain.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mts.crudprojetosweb.domain.exception.NegocioException;
 import com.mts.crudprojetosweb.domain.model.Pessoa;
 import com.mts.crudprojetosweb.domain.repository.PessoaRepository;
 
@@ -17,6 +18,14 @@ public class PessoaService {
 	
 	@Transactional
 	public Pessoa salvar(Pessoa pessoa) {
+		boolean emailEmUso = pessoaRepository.findByEmail(pessoa.getEmail()) //regra para não criar pessoa com email repetido
+				.stream() //regra para não criar pessoa com email repetido
+				.anyMatch(pessoaExistente -> !pessoaExistente.equals(pessoa)); //regra para não criar pessoa com email repetido
+		
+		if(emailEmUso) {
+			throw new NegocioException("Já existe uma pessoa cadastrada com esse e-mail.");
+		}
+		
 		return pessoaRepository.save(pessoa);
 	}
 	
